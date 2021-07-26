ea5d5166a9035b029f088f088f24730d7abad9df
BootCampBL <satyabhandari43@gmail.com>
2021-07-26 18:52:09 +0530

    [sathya] Added - Checking Last Name is Valid or Not

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -8,6 +8,7 @@ namespace DAY_19_UserRegistration
     public class UserRegistration
     {
         public static string Regex_FirstName = "^[A-Z][a-z]{2,}$";
+        public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
         public static bool ValidateFirstName(string FirstName)
         {
 
@@ -27,6 +28,23 @@ namespace DAY_19_UserRegistration
             {
                 Console.WriteLine("is valid ");
             }
+        }
+        public static bool ValidateLastName(string LastName)
+        {
+
+
+            return Regex.IsMatch(LastName, Regex_LastName);
+
+        }
+        public static void CheckLastName(string LastName)
+        {
+            if (Regex.IsMatch(LastName, Regex_LastName) == false)
+            {
+                Console.WriteLine("Enter valid LastName in proper format ");
+
+            }
+
+
         }
     }
 }
